e960aa25f376f9d3b86c471cfcad7ed6f78976f4
Luqui87 <alvarezlucas2787@gmail.com>
2023-05-12 14:35:20 -0300

    Health bar agregada

---
+++ src/screens/MainMenu.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func _ready():
+	$Container/Buttons/Start.grab_focus()
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://src/game/levels/LevelManager.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
